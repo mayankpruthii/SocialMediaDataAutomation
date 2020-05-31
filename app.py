c21b0f89b5436fe4b5a234ed883d091e6f1667d7
Mayank <mayankpruthi123@gmail.com>
2020-06-01 00:32:28 +0530

    Works with Medium and Insta

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from dictsheet import DictSheet
 from TwitterDataFetch import TwitterDataFetch
 from FacebookDataFetch import FacebookInsightsEnglishSite
 from FacebookDataFetch import FacebookInsightsHindiSite
+from InstagramDataFetch import InstagramInsights
+from MediumStats import MediumStatsScraper
 
 with open('./Cred.json') as f:
         data = json.load(f)
@@ -24,6 +26,16 @@ twitterWks = my_spreadsheet.worksheet('Twitter')
 dict_TwitterWks = DictSheet(wks = twitterWks)
 dict_TwitterWks.append(twitter_data)
 
+print('Twitter Scraping Done!')
+
+medium_data = MediumStatsScraper()
+print(medium_data)
+mediumWks = my_spreadsheet.worksheet('Medium')
+dict_mediumWks = DictSheet(wks = mediumWks)
+dict_mediumWks.append(medium_data)
+
+print('Medium Scraping Done!')
+
 facebook_data_english = FacebookInsightsEnglishSite()
 print(facebook_data_english)
 FacebookWks = my_spreadsheet.worksheet('Facebook')
@@ -35,5 +47,11 @@ print(facebook_data_hindi)
 FacebookHindiWks = my_spreadsheet.worksheet('FacebookHindi')
 dict_FacebookWks = DictSheet(wks = FacebookHindiWks)
 dict_FacebookWks.append(facebook_data_hindi)
+
+instagram_data = InstagramInsights()
+print(instagram_data)
+InstagramWks = my_spreadsheet.worksheet('Instagram')
+dict_InstagramWks = DictSheet(wks = InstagramWks)
+dict_InstagramWks.append(instagram_data)
 
 print('Done!')
